80c373a8d836680f0a682f477e4fe5ff52e30857
mr-artur <ar2r.pv@gmail.com>
2020-03-01 15:20:50 +0200

    added showing alerts depending on response

--- src/main/resources/static/js/form.js
+++ src/main/resources/static/js/form.js
@@ -1,9 +1,10 @@
+const container = document.getElementById("mainContainer");
 const form = document.getElementById("form");
 
 const handleSubmit = e => {
     e.preventDefault();
     const data = serializeData();
-    const { dataset: { url } } = form;
+    const {dataset: {url}} = form;
     sendForm(url, data);
 }
 
@@ -12,13 +13,13 @@ const serializeData = () => {
     const parameters = [];
 
     for (let i = 0; i < inputs.length; i++) {
-        parameters[i] = inputs[i].name + "=" + inputs[i].value ;
+        parameters[i] = inputs[i].name + "=" + inputs[i].value;
     }
     return parameters.join("&");
 }
 
-const sendForm = async (url, data) => {
-    const response = await fetch(
+const sendForm = (url, data) => {
+    fetch(
         url,
         {
             method: "POST",
@@ -27,8 +28,31 @@ const sendForm = async (url, data) => {
             },
             body: data
         }
+    ).then(
+        data => showSuccess(),
+        error => showError()
     );
-    console.log(response);
+}
+
+const showSuccess = () => {
+    const alert = buildAlert("success", "Data submit was successful");
+    container.appendChild(alert);
+    setTimeout(() => container.removeChild(alert), 2500);
+}
+
+const buildAlert = (type, message) => {
+    const div = document.createElement("div");
+    div.innerText = message;
+    div.classList.add("alert", `alert-${type}`, "mt-4");
+    div.style.margin = "auto";
+    div.style.maxWidth = "250px";
+    return div;
+}
+
+const showError = () => {
+    const alert = buildAlert("danger", "Error in data submitting");
+    container.appendChild(alert);
+    setTimeout(() => container.removeChild(alert), 2500);
 }
 
 const init = () => {
