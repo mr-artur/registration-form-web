a6203a1b58441093ddeca7d600300c62a077d947
mr-artur <ar2r.pv@gmail.com>
2020-03-06 00:20:53 +0200

    formatted html

--- src/main/java/ua/kpi/arturo/registrationform/dto/NativeUserDto.java
+++ src/main/java/ua/kpi/arturo/registrationform/dto/NativeUserDto.java
@@ -1,15 +1,11 @@
 package ua.kpi.arturo.registrationform.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import ua.kpi.arturo.registrationform.entity.RoleType;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 @Setter
 @Getter
--- src/main/java/ua/kpi/arturo/registrationform/entity/User.java
+++ src/main/java/ua/kpi/arturo/registrationform/entity/User.java
@@ -1,6 +1,11 @@
 package ua.kpi.arturo.registrationform.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
